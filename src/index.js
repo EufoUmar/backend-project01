8dac5afe2454fc05962fa9b1e779bbcd27030e30
Muhammad Umar <umar.eufo@gmail.com>
2024-10-24 21:13:03 +0500

    day2 finshed

--- src/index.js
+++ src/index.js
@@ -1,32 +1,21 @@
 
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({
     path: "./env"
 })
 
-connectDB()
+connectDB().then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server Started At Port`)
+    })
+}).catch((error) => {
+    console.log("Server Error", error);
+})
 
-// const app = express()
 
-// (async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("Errors", (error) => {
-//            console.log("ERRS", error);
-//            throw error
-//         });
-// //        application listening ------------
-//        app.listen(process.env.PORT, () => {
-//          console.log(`Server Started At PORT 8000 ${process.env.PORT}`);
-         
-//        })
-//     } catch (error) {
-//         console.error("Error", error);
-//         throw error
-//     }
-// } )()
 
 
     
