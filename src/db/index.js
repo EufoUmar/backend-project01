b6a6ad0ab45d839b11c88fbda53ca0b0f4a43123
Muhammad Umar <umar.eufo@gmail.com>
2024-10-25 21:15:40 +0500

    add utils day3 comp

--- src/db/index.js
+++ src/db/index.js
@@ -1,24 +1,13 @@
-import express from "express";
 import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
-// const DB_NAME = require("../constants")
-import {DB_NAME} from "../constants.js";
 
-const app = express()
-
-const connectDB = async() => {
+const connectDB = async () => {
     try {
-       const response = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-    
-           console.log(`\n mongoDB connected || DB Host ${response.connection.host}`)
-
-//        application listening ------------
-       app.listen(process.env.PORT, () => {
-         console.log(`Server Started At PORT 8000 ${process.env.PORT}`);
-         
-       })
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MongoDB Connection Error", error);
+        console.log("MONGODB connection FAILED ", error);
         process.exit(1)
     }
 }
